72197ec8205ada55b731aa51ae924f4d85a0c534
Amanpreet Singh <apsdehal@gmail.com>
2018-04-18 15:56:22 -0400

    Skip frames using custom skipper, allow unit instantiation in specific places
    - Fix done function call

--- gym_starcraft/envs/starcraft_base_env.py
+++ gym_starcraft/envs/starcraft_base_env.py
@@ -36,6 +36,8 @@ class StarCraftBaseEnv(gym.Env):
         self.speed = speed
         self.max_episode_steps = max_episode_steps
         self.set_gui = set_gui
+        self.frame_count = 0
+
 
         self.server_port = self._find_available_port(server_ip, server_port)
 
@@ -54,7 +56,7 @@ class StarCraftBaseEnv(gym.Env):
             options[key] = str(val)
 
         options['BWAPI_CONFIG_AUTO_MENU__GAME_TYPE'] = "USE MAP SETTINGS"
-        options['BWAPI_CONFIG_AUTO_MENU__AUTO_RESTART'] = "OFF"
+        options['BWAPI_CONFIG_AUTO_MENU__AUTO_RESTART'] = "ON"
         options['TORCHCRAFT_PORT'] = str(self.server_port)
 
         cmds.append(self.bwapi_launcher_path)
@@ -65,19 +67,26 @@ class StarCraftBaseEnv(gym.Env):
 
         time.sleep(5)
 
-
         self.episodes = 0
         self.episode_wins = 0
         self.episode_steps = 0
         self.map = 'maps/micro/micro-empty2.scm'
         self.first_reset = True
 
-
+        # TODO: Move them in proper setters and getters
         # NOTE: These should be overrided in derived class
-        # Should be a list of pairs where each pair is (number, unit_type)
+        # Should be a list of pairs where each pair is
+        # (quantity, unit_type, x, y, start_coordinate, end_coordinate)
+        # So (1, 0, -1, -1, 100, 150) will instantiate 0 type unit
+        # at a random place between x = (100, 150) and y = (100, 150)
+        # Leave empty if you want to instantiate anywhere in whole map
         self.my_unit_pairs = []
         self.enemy_unit_pairs = []
 
+        self.my_current_units = {}
+        self.enemy_current_units = {}
+        self.agent_ids = []
+        self.enemy_ids = []
         self.state = None
         self.obs = None
         self.obs_pre = None
@@ -93,7 +102,9 @@ class StarCraftBaseEnv(gym.Env):
         setup = [[tcc.set_combine_frames, 1],
                  [tcc.set_speed, self.speed],
                  [tcc.set_gui, self.set_gui],
-                 [tcc.set_frameskip, self.frame_skip],
+                 # NOTE: We use custom frameskip method now
+                 # Skip frame below
+                 [tcc.set_frameskip, 1],
                  [tcc.set_cmd_optim, 1]]
 
         self.client.send(setup)
@@ -137,6 +148,8 @@ class StarCraftBaseEnv(gym.Env):
         self.client.send(self._make_commands(action))
         self.state = self.client.recv()
 
+        self._skip_frames()
+
         while not self._has_step_completed():
             self.client.send([])
             self.state = self.client.recv()
@@ -148,10 +161,16 @@ class StarCraftBaseEnv(gym.Env):
         self.obs_pre = self.obs
         return self.obs, reward, done, info
 
+    def _skip_frames(self):
+        count = 0
+        while count < self.frame_skip:
+            self.client.send([])
+            self.state = self.client.recv()
+            count += 1
+
     def try_killing(self):
         if not self.state:
             return
-
 
         while len(self.state.aliveUnits.values()) != 0:
             command = []
@@ -184,16 +203,14 @@ class StarCraftBaseEnv(gym.Env):
         if self.first_reset:
             self.init_conn()
             self.first_reset = False
-        
+
         self.try_killing()
 
         for unit_pair in self.my_unit_pairs:
-            command += self.create_units(0, unit_pair[1], x=unit_pair[2],
-                                         y=unit_pair[3], unit_type=unit_pair[0])
+            command += self._get_create_units_command(0, unit_pair)
 
         for unit_pair in self.enemy_unit_pairs:
-            command += self.create_units(1, unit_pair[1], x=unit_pair[2],
-                                         y=unit_pair[3], unit_type=unit_pair[0])
+            command += self._get_create_units_command(1, unit_pair)
 
         if len(command):
             self.client.send(command)
@@ -203,18 +220,32 @@ class StarCraftBaseEnv(gym.Env):
             self.client.send([])
             self.state = self.client.recv()
 
+        # This adds my_units and enemy_units to object.
+        self.my_current_units = self._parse_units_to_unit_dict(self.state.units[0])
+        self.enemy_current_units = self._parse_units_to_unit_dict(self.state.units[1])
+
+        # This adds my and enemy's units' ids as incrementing list
+        self.agent_ids = list(self.my_current_units)
+        self.enemy_ids = list(self.enemy_current_units)
 
         self.obs = self._make_observation()
         self.obs_pre = self.obs
         return self.obs
 
+    def _get_create_units_command(self, player_id, unit_pair):
+        defaults = [1, 100, 100, 0, self.state.map_size[0] - 10][len(unit_pair) - 1:]
+        unit_type, quantity, x, y, start, end = (list(unit_pair) + defaults)[:6]
 
-    def create_units(self, player_id, quantity, x=100, y=100, unit_type=0):
+        return self.create_units(player_id, quantity, x=x, y=y,
+                                 unit_type=unit_type, start=start,
+                                 end=end)
+
+    def create_units(self, player_id, quantity, unit_type=0, x=100, y=100, start=0, end=256):
         if x < 0:
-            x = random.randint(0, self.state.map_size[0]) * DISTANCE_FACTOR
-        
+            x = (random.randint(0, end - start) + start) * DISTANCE_FACTOR
+
         if y < 0:
-            y = random.randint(0, self.state.map_size[1]) * DISTANCE_FACTOR
+            y = (random.randint(0, end - start) + start) * DISTANCE_FACTOR
         commands = []
 
         for _ in range(quantity):
@@ -247,6 +278,15 @@ class StarCraftBaseEnv(gym.Env):
             commands.append(command)
         return commands
 
+
+    def _parse_units_to_unit_dict(self, units, units_type='my_units'):
+        unit_dict = dict()
+
+        for unit in units:
+            unit_dict[unit.id] = unit
+
+        return unit_dict
+
     def _action_space(self):
         """Returns a space object"""
         raise NotImplementedError
@@ -274,7 +314,11 @@ class StarCraftBaseEnv(gym.Env):
 
     def _check_done(self):
         """Returns true if the episode was ended"""
-        return (bool(self.state.game_ended) or self.state.battle_just_ended) and (len(self.state.units[0]) == 0 or len(self.state.units[1]) == 0)
+        return (bool(self.state.game_ended) or \
+                self.state.battle_just_ended) or \
+                len(self.state.units[0]) == 0 or \
+                len(self.state.units[1]) == 0 or \
+                self.episode_steps == self.max_episode_steps
 
     def _get_info(self):
         """Returns a dictionary contains debug info"""
