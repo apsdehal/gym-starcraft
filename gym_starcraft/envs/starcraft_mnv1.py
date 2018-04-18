0329503509e5355633d94ba9a9473588cb347ca6
Amanpreet Singh <apsdehal@gmail.com>
2018-04-18 15:59:47 -0400

    New reward structure and vision fixes

--- gym_starcraft/envs/starcraft_mnv1.py
+++ gym_starcraft/envs/starcraft_mnv1.py
@@ -20,16 +20,16 @@ class StarCraftMNv1(sc.StarCraftBaseEnv):
                                               args.server_port, args.speed,
                                               args.frame_skip, args.set_gui, args.self_play,
                                               args.max_steps, final_init)
-        
+
         self.nagents = args.nagents
-        self.my_unit_pairs = [(0, 1, -1, -1) for _ in range(args.nagents)]
+        self.my_unit_pairs = [(0, 1, -1, -1, 100, 150) for _ in range(args.nagents)]
 
-        self.enemy_unit_pairs = [(0, 1, -1, -1)]
+        self.enemy_unit_pairs = [(0, 1, -1, -1, 100, 150)]
 
-        self.max_frame_skip = 10
         self.vision = 5
-        self.frame_count = 0
         self.move_steps = ((0, 1), (0, -1), (-1, 0), (1, 0), (0, 0))
+
+        self.prev_actions = np.zeros(self.nagents)
 
     def _action_space(self):
         # Move up, down, left, right, stay, attack agents i to n
@@ -39,9 +39,9 @@ class StarCraftMNv1(sc.StarCraftBaseEnv):
         return spaces.MultiDiscrete([self.nactions])
 
     def _observation_space(self):
-        # relative_x, relative_y, my_hp, enemy_hp, my_cooldown, enemy_cooldown
-        obs_low = [-1.0, -1.0, 0.0, 0.0, 0.0, 0.0]
-        obs_high = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+        # relative_x, relative_y, in_vision, my_hp, enemy_hp, my_cooldown, enemy_cooldown
+        obs_low = [-1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        obs_high = [1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0]
 
         return spaces.Box(np.array(obs_low), np.array(obs_high), dtype=np.float32)
 
@@ -54,12 +54,18 @@ class StarCraftMNv1(sc.StarCraftBaseEnv):
         if len(self.state.units[0]) > self.nagents:
             return cmds
 
-        enemy_units = self.state.units[1]
         enemy_unit = None
 
-        for idx, unit in enumerate(self.state.units[0]):
-            my_unit = unit
+        for idx in range(self.nagents):
+            agent_id = self.agent_ids[idx]
+
+            if agent_id not in self.my_current_units:
+                # Agent is probably dead
+                continue
+
+            my_unit = self.my_current_units[agent_id]
             action = actions[idx]
+            prev_action = self.prev_actions[idx]
 
             if action < len(self.move_steps):
                 x2 = my_unit.x + self.move_steps[action][0]
@@ -70,58 +76,72 @@ class StarCraftMNv1(sc.StarCraftBaseEnv):
                     tcc.unitcommandtypes.Move, -1, int(x2), int(y2), -1
                 ])
             else:
-                agent_id = action - len(self.move_steps)
+                enemy_id = action - len(self.move_steps)
 
-                if len(enemy_units) > agent_id:
-                    enemy_unit = enemy_units[agent_id]
+                if len(self.enemy_ids) > enemy_id:
+                    enemy_id = self.enemy_ids[enemy_id]
+                    enemy_unit = self.enemy_current_units[enemy_id]
                 else:
                     enemy_unit = None
-                
+
                 if not enemy_unit:
                     continue
 
                 distance = utils.get_distance(my_unit.x, -my_unit.y,
                                               enemy_unit.x, -enemy_unit.y)
 
-                if distance < my_unit.groundRange:
+                unit_command = tcc.command_unit_protected
+
+                if prev_action < len(self.move_steps):
+                    unit_command = tcc.command_unit
+
+                if distance <= my_unit.groundRange:
                     cmds.append([
-                        tcc.command_unit_protected, my_unit.id,
+                        unit_command, my_unit.id,
                         tcc.unitcommandtypes.Attack_Unit, enemy_unit.id
                     ])
+        self.prev_actions = actions
         return cmds
 
     def _has_step_completed(self):
         check = True
-        if self.frame_count < self.max_frame_skip:
-            check = False
-            self.frame_count += 1
-        else:
-            self.frame_count = 0
         return check
 
     def _make_observation(self):
         myself = None
         enemy = None
 
-        for unit in self.state.units[1]:
-            enemy = unit
+        for enemy_id in self.enemy_ids:
+            enemy = self.enemy_current_units[enemy_id]
 
         full_obs = []
+
         for idx in range(self.nagents):
-            if len(self.state.units[0]) > idx:
-                myself = self.state.units[0][idx]
+            agent_id = self.agent_ids[idx]
+
+            if agent_id in self.my_current_units:
+                myself = self.my_current_units[agent_id]
             else:
                 myself = None
 
             curr_obs = np.zeros(self.observation_space.shape)
 
             if myself is not None and enemy is not None:
-                curr_obs[0] = (myself.x - enemy.x) / (self.state.map_size[0])
-                curr_obs[1] = (myself.y - enemy.y) / (self.state.map_size[0])
-                curr_obs[2] = myself.health / myself.max_health
-                curr_obs[3] = enemy.health / enemy.max_health
-                curr_obs[4] = myself.groundCD / myself.maxCD
-                curr_obs[5] = enemy.groundCD / enemy.maxCD
+                distance = utils.get_distance(myself.x, myself.y, enemy.x, enemy.y)
+
+                if distance < self.vision:
+                    curr_obs[0] = (myself.x - enemy.x) / (self.vision)
+                    curr_obs[1] = (myself.y - enemy.y) / (self.vision)
+                    curr_obs[2] = 0
+                else:
+                    curr_obs[0] = 0
+                    curr_obs[1] = 0
+                    curr_obs[2] = 1
+
+                curr_obs[3] = myself.health / myself.max_health
+                curr_obs[4] = enemy.health / enemy.max_health
+                curr_obs[5] = myself.groundCD / myself.maxCD
+                curr_obs[6] = enemy.groundCD / enemy.maxCD
             full_obs.append(curr_obs)
 
         return np.stack(full_obs)
@@ -130,20 +150,28 @@ class StarCraftMNv1(sc.StarCraftBaseEnv):
         reward = np.full(self.nagents, self.TIMESTEP_PENALTY)
 
         for idx in range(len(self.obs)):
-            reward[idx] += self.obs[idx][3] - self.obs[idx][2]
-            reward[idx] += -0.1
-
-            if self._check_done() and not bool(self.state.battle_won):
-                reward[idx] += -100
+            if self.episode_steps == self.max_episode_steps:
+                reward[idx] += 0 - self.obs_pre[idx][3]
+            else:
+                reward[idx] += self.obs_pre[idx][3] - self.obs[idx][3]
+                reward[idx] += self.obs[idx][4] - self.obs_pre[idx][4]
 
             if self._check_done() and bool(self.state.battle_won):
-                reward[idx] += +100
+                reward[idx] += +10
                 self.episode_wins += 1
 
-            if self.episode_steps == self.max_episode_steps:
-                reward[idx] += -100
-
         return reward
+
+    def _get_info(self):
+        alive_mask = np.ones(self.nagents)
+
+        for idx in range(self.nagents):
+            agent_id = self.agent_ids[idx]
+
+            if agent_id not in self.my_current_units:
+                alive_mask[idx] = 0
+
+        return {'alive_mask': alive_mask}
 
     def step(self, action):
         return self._step(action)
